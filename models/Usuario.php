b884c243bc4f9234927520e7ccc4f08f23445d4c
Carlos Adiel Reyes Soto <carlosreyes1803@gmail.com>
2023-10-02 06:58:56 -0600

    Se corrigio el Model

--- models/Usuario.php
+++ models/Usuario.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace Model;
 
-class  Usuario extends ActiveRecord{
+class Usuario extends ActiveRecord{
     protected static $tabla = 'usuario';
-    protected static $columnasDB = ['usu_nombre','usu_dpi','usu_password','usu_email','usu_telefono', 'usu_rol', 'usu_situacion'];
+    protected static $columnasDB = ['usu_nombre','usu_dpi','usu_password','usu_email','usu_telefono','usu_rol','usu_situacion'];
     protected static $idTabla = 'usu_id';
-
 
     public $usu_id;
     public $usu_nombre;
@@ -13,10 +13,10 @@ class  Usuario extends ActiveRecord{
     public $usu_password;
     public $usu_email;
     public $usu_telefono;
-  
+    public $usu_rol;
     public $usu_situacion;
-    
-    public function __construct($args =[])
+
+    public function __construct($args = [])
     {
         $this->usu_id = $args['usu_id'] ?? null;
         $this->usu_nombre = $args['usu_nombre'] ?? '';
@@ -24,9 +24,7 @@ class  Usuario extends ActiveRecord{
         $this->usu_password = $args['usu_password'] ?? '';
         $this->usu_email = $args['usu_email'] ?? '';
         $this->usu_telefono = $args['usu_telefono'] ?? '';
-        $this->usu_situacion = $args['usu_situacion'] ?? '1';
-        
+        $this->usu_rol = $args['usu_rol'] ?? '4';
+        $this->usu_situacion = $args['usu_situacion'] ?? '2';
     }
 }
-
-?>
