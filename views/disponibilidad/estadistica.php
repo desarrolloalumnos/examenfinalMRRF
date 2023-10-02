ecfac76f56087779a09f2a673afb137d701daad1
lestermaudiel <flestermaudiel2130@gmail.com>
2023-10-02 02:39:06 -0600

    se agrego boton para poder regresar de estadisticas a menu.

--- views/disponibilidad/estadistica.php
+++ views/disponibilidad/estadistica.php
@@ -4,4 +4,10 @@
         <canvas id="chartdisponibilidad" width="100%"></canvas>
     </div>
 </div>
+<button id="regresarBtn" style="background-color: red; color: white;">Regresar</button>
 <script src="<?=asset('./build/js/disponibilidad/estadistica.js')?>"></script>
+<script>
+    document.getElementById('regresarBtn').addEventListener('click', function() {
+        window.location.href = '/examenfinalMRRF/menuAdministrador';
+    });
+</script>
