76d0b57f7e0c6abdc23895e29419c16a6c381161
Carlos Adiel Reyes Soto <carlosreyes1803@gmail.com>
2023-10-02 10:45:08 -0600

    Se subieron los controladores

---
+++ controllers/ActivacionController.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Controllers;
+
+use Exception;
+use Model\Usuario;
+use Model\Rol;
+use MVC\Router;
+
+class ActivacionController {
+    public static function index(Router $router){
+
+        $roles = static::buscarRoles();
+
+        $usuarios = Usuario::all();
+
+        $router->render('activacion/index', [
+            'activacion' => $activacion,
+            'roles' => $roles,
+        ]);
+    }
+
+    //!--------------------------
+public static function buscarRoles(){
+    $sql = "SELECT * FROM rol where rol_situacion = 1";
+
+    try {
+        $roles = Usuario::fetchArray($sql);
+
+        if($roles){
+            return $roles;
+        }else{
+            return 0;
+        }
+    } catch (Exception $e) {
+        
+    }
+}
+
+    //!Funcion Buscar
+    public static function buscarAPI()
+    {
+
+        $sql = "SELECT u.usu_id, u.usu_nombre, u.usu_dpi, u.usu_password, u.usu_email, u.usu_telefono, r.rol_nombre
+        FROM usuario u
+        LEFT JOIN rol r ON u.usu_rol = r.rol_id
+        WHERE u.usu_situacion = 2;
+         ";
+
+        try {
+
+            $usuarios = Usuario::fetchArray($sql);
+
+            echo json_encode($usuarios);
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+    }
+
+    public static function activarAPI(){
+        try {
+            $usu_id = $_POST['usu_id'];
+            $usuario = Usuario::find($usu_id);
+    
+            // Verificar si el rol es "PENDIENTE"
+            if ($usuario->usu_rol === '4') {
+                echo json_encode([
+                    'mensaje' => 'No se puede activar el usuario con rol "PENDIENTE"',
+                    'codigo' => 2
+                ]);
+                return;
+            }
+    
+    
+            $usuario->usu_situacion = 1;
+            $resultado = $usuario->actualizar();
+    
+            if ($resultado['resultado'] == 1) {
+                echo json_encode([
+                    'mensaje' => 'Usuario Activado Exitosamente',
+                    'codigo' => 1
+                ]);
+            } else {
+                echo json_encode([
+                    'mensaje' => 'Ocurrio un error',
+                    'codigo' => 0
+                ]);
+            }
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrio un Error',
+                'codigo' => 0
+            ]);
+        }
+    }
+    
+    
+
+    //!Funcion Eliminar
+    public static function eliminarAPI(){
+        try{
+            $usu_id = $_POST['usu_id'];
+            $usuario = Usuario::find($usu_id);
+            $usuario->usu_situacion = 0;
+            $resultado = $usuario->actualizar();
+
+            if($resultado['resultado'] == 1){
+                echo json_encode([
+                    'mensaje' => 'Registro guardado correctamente',
+                    'codigo' => 1
+                ]);
+            }else{
+                echo json_encode([
+                    'mensaje' => 'Ocurrio un error',
+                    'codigo' => 0
+                ]);
+            }
+        }catch(Exception $e){
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje'=> 'Ocurrio un Error',
+                'codigo' => 0
+        ]);
+        }
+    }
+
+    public static function asignarolAPI() {
+        try {
+            $usuarioData = $_POST;
+    
+            // Validar campos vacíos
+            foreach ($usuarioData as $campo => $valor) {
+                if (empty($valor)) {
+                    echo json_encode([
+                        'mensaje' => 'Debe llenar todos los campos',
+                        'codigo' => 0
+                    ]);
+                    return;
+                }
+            }
+    
+            $usuario = new Usuario($usuarioData);
+            $resultado = $usuario->actualizar();
+    
+            if ($resultado['resultado'] == 1) {
+                echo json_encode([
+                    'mensaje' => 'Asignación de Rol Correcta',
+                    'codigo' => 1
+                ]);
+            } else {
+                echo json_encode([
+                    'mensaje' => 'Ocurrió un error',
+                    'codigo' => 0
+                ]);
+            }
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un Error',
+                'codigo' => 0
+            ]);
+        }
+    }
+    
+}
---
+++ controllers/HabitacionController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Controllers;
+
+use Exception;
+use Model\Habitacion;
+use MVC\Router;
+
+class HabitacionController {
+    public static function index(Router $router){
+        $router->render('habitaciones/habitacionesadmin/index', []);
+    }
+    public static function indexclientes(Router $router){
+        $router->render('habitaciones/habitacionesclientes/index', []);
+    }
+    public static function indexempleados(Router $router){
+        $router->render('habitaciones/habitacionesempleados/index', []);
+    }
+ 
+    // public static function indexreservaciones(Router $router){
+    //     $router->render('reservaciones/index', []);
+    // }
+
+
+    public static function guardarApi(){
+     
+        try {
+            $habitacion = new Habitacion($_POST);
+            $resultado = $habitacion->crear();
+
+            if ($resultado['resultado'] == 1) {
+                echo json_encode([
+                    'mensaje' => 'Registro guardado correctamente',
+                    'codigo' => 1
+                ]);
+            } else {
+                echo json_encode([
+                    'mensaje' => 'Ocurrió un error',
+                    'codigo' => 0
+                ]);
+            }
+            // echo json_encode($resultado);
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+    }
+
+
+    public static function buscarAPI() {
+        $habitacion_numero = $_GET['habitacion_numero'] ?? '';
+        $habitacion_tipo = $_GET['habitacion_tipo'] ?? '';
+        $habitacion_descripcion = $_GET['habitacion_descripcion'] ?? '';
+        $precio_minimo = $_GET['precio_minimo'] ?? null;
+        $precio_maximo = $_GET['precio_maximo'] ?? null;
+    
+        $sql = "SELECT * FROM habitaciones WHERE habitacion_situacion = 1 ";
+        
+        if ($habitacion_numero != '') {           
+            $sql .= " AND TO_CHAR(habitacion_numero) LIKE '%$habitacion_numero%' ";
+        }
+
+        if ($habitacion_tipo != '') {
+            //Sanitizar y validar $habitacion_tipo si es necesario
+            $habitacion_tipo = strtolower($habitacion_tipo);
+            $sql .= " AND LOWER(habitacion_tipo) LIKE '%$habitacion_tipo%' ";
+        }
+            
+        if ($habitacion_descripcion != '') {
+            //Sanitizar y validar $habitacion_descripcion si es necesario
+            $habitacion_descripcion = strtolower($habitacion_descripcion);
+            $sql .= " AND LOWER(habitacion_descripcion) LIKE '%$habitacion_descripcion%' ";
+        }
+            
+        if ($precio_minimo !== null && $precio_maximo !== null) {
+            $sql .= " AND habitacion_tarifa BETWEEN $precio_minimo AND $precio_maximo ";
+        } elseif ($precio_minimo !== null) {
+            $sql .= " AND habitacion_tarifa >= $precio_minimo ";
+        } elseif ($precio_maximo !== null) {
+            $sql .= " AND habitacion_tarifa <= $precio_maximo ";
+        }
+    
+        try {
+            $habitaciones = Habitacion::fetchArray($sql);
+            echo json_encode($habitaciones);
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+    }
+
+
+    public static function modificarAPI()
+    {
+        try {
+            $habitacion = new Habitacion($_POST);
+            $resultado = $habitacion->actualizar();
+
+            if ($resultado['resultado'] == 1) {
+                echo json_encode([
+                    'mensaje' => 'Registro modificado correctamente',
+                    'codigo' => 1
+                ]);
+            } else {
+                echo json_encode([
+                    'mensaje' => 'Ocurrió un error',
+                    'codigo' => 0
+                ]);
+            }
+            // echo json_encode($resultado);
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+    }
+
+    public static function eliminarAPI()
+    {
+        try {
+            $habitacion_id = $_POST['habitacion_id'];
+            $habitacion = Habitacion::find($habitacion_id);
+            $habitacion->habitacion_situacion = 0;
+            $resultado = $habitacion->actualizar();
+
+            if ($resultado['resultado'] == 1) {
+                echo json_encode([
+                    'mensaje' => 'Registro eliminado correctamente',
+                    'codigo' => 1
+                ]);
+            } else {
+                echo json_encode([
+                    'mensaje' => 'Ocurrió un error',
+                    'codigo' => 0
+                ]);
+            }
+            // echo json_encode($resultado);
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
+    }
+}
