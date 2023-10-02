dbb1b8d2c871dcde7d5219e7b046f4d0de6b2992
Carlos Adiel Reyes Soto <carlosreyes1803@gmail.com>
2023-10-02 10:43:56 -0600

    se eliminaron archivos no funconales

--- views/disponibilidad/estadistica.php
+++
@@ -1,7 +0,0 @@
-<h1>DISPONIBILIDAD DE HABITACIONES</h1>
-<div class="row">
-    <div class="col-lg-9">
-        <canvas id="chartdisponibilidad" width="100%"></canvas>
-    </div>
-</div>
-<script src="<?=asset('./build/js/disponibilidad/estadistica.js')?>"></script>
